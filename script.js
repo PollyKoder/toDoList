e2bab0d33d87bc12ece92b5a4a4f8913960f3ffa
Polly <riviera.koder@gmail.com>
2023-10-23 15:27:28 +0200

    edit5

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+// The elements from html that is needed to interact
 const textInput = document.querySelector("#textInputID");
 const taskList = document.querySelector("#listTaskID");
 const toDoForm = document.querySelector("#toDoFormID");
+
+//add a new task to the task list
+
+function addTask() {
+    if (textInput.value) {
+        //if input
+        const newTask = document.createElement("li");
+        const deleteButton = document.createElement("button");
+    }
+}
