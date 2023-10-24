042d29b50e1b84a6523847dd57ae49345804654e
Polly <riviera.koder@gmail.com>
2023-10-24 09:54:50 +0200

    edit6

--- README.md
+++ README.md
@@ -6,3 +6,5 @@
 
 # My Notes:
 - Start by making basic html with elements needed to construct the To-do list. Elements will in this case be: semantic setup(body, header, main and section), div, form, input and button. Accompanied by relevant text.
+- Have added some simple css and a background picture.
+- Started on the js to manipulate the html, tried to write the code in a manner that I find more organized, but now I don't know if that's why the code doesn't seem to work, will have to do some console logs to see if I can find the point where the code stops functioning. What's odd is that it doesn't give me an error in the  console i dev tool.
--- script.js
+++ script.js
@@ -3,12 +3,33 @@ const textInput = document.querySelector("#textInputID");
 const taskList = document.querySelector("#listTaskID");
 const toDoForm = document.querySelector("#toDoFormID");
 
-//add a new task to the task list
+
+//add a new task to the task list and make it possible to delete it again
 
 function addTask() {
     if (textInput.value) {
         //if input
         const newTask = document.createElement("li");
         const deleteButton = document.createElement("button");
+        deleteButton.textContent = "❌";
+
+        newTask.textContent = textInput.value;
+        taskList.appendChild(newTask);
+        newTask.appendChild(deleteButton);
+    } else {
+        //if no input
+        alert("Please enter a task")
     }
+    saveData();
+}
+
+//the function below removes the task from the tasklist when it's assigned button is clicked
+taskList.addEventListener("click", function (event) {
+    event.target.tagName === "BUTTON" && event.target.parentElement.remove();
+    saveData();
+});
+
+//Saves the data to local storage, is fired by the add data function
+function saveData() {
+    localStorage.setItem("taskList". taskList.innerHTML);
 }
